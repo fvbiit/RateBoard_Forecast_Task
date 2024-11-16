9d11a3162397478f1c5968e10fbb76617b6fd359
fvbiit <ftschuggmall@gmail.com>
2024-11-16 21:53:25 +0100

    Forecast Logic and DB Configuration

--- RateBoard_Forecast_Task/Forecast_db/models.py
+++ RateBoard_Forecast_Task/Forecast_db/models.py
@@ -1,11 +1,15 @@
-from django.db import models
+from django.db import models #Libraries
 
-# Create your models here.
-
+"""
+Create a Model-Class "Forecast" for Database and define the behaviour for the Datasets. 
+"""
 class Forecast(models.Model):
+
+    #Datasets
     company_id = models.IntegerField()
     forecast_date = models.DateField()
-    values = models.JSONField()
+    values = models.JSONField() #Create List or Dict
 
+    #Str Function for Object Output.
     def __str__(self):
         return f"ID: {self.id}, Company_ID: {self.company_id}, Forecast Date: {self.forecast_date}, Values: {self.values}"
--- RateBoard_Forecast_Task/Forecast_db/views.py
+++ RateBoard_Forecast_Task/Forecast_db/views.py
@@ -5,14 +5,38 @@ Views are Python functions that take requests and return responses.
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Forecast
+from datetime import datetime
 
 # Create your views here.
+
 def forecast_view(request):
-    
-    forecasts = Forecast.objects.all()
+
+    #fetches the query param defined in postman
+    comp_id = request.GET.get('company_id')
+    date_str = request.GET.get('date')
+
+
+    forecasts = Forecast.objects.all() #Problem if there are many datasets (Performance)
     datalist=[]
-    for dataset in forecasts:
-        data = {"id":dataset.id, "company_id": dataset.company_id, "forecast_date":dataset.forecast_date, "values": dataset.values}   
-        datalist.append(data)
+
+    try:
+        conv_comp_id = int(comp_id)
+        conv_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+
+        for dataset in forecasts:
+            if conv_comp_id == dataset.company_id and dataset.forecast_date <= conv_date:
+                date_offset = (conv_date - dataset.forecast_date).days
+                
+                for filtered_values in range(date_offset, len(dataset.values)):
+                    data = dataset.values[filtered_values]
+                    datalist.extend({data})
+
+        if not datalist:
+            return JsonResponse({"Values": "No Forecast Values found"})
+        
+        return JsonResponse({"Values": datalist})
     
-    return JsonResponse({"datasets": datalist})
+    except ValueError:
+        return JsonResponse({"Values": "Your input was not valid"})
+    
+    
